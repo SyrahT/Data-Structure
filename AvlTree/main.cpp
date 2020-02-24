7c689c461f367e548700b8e6254f065f291062cd
SyrahT <32165591+SyrahT@users.noreply.github.com>
2020-02-24 11:54:26 +0800

    Add files via upload

--- AvlTree/main.cpp
+++ AvlTree/main.cpp
@@ -8,7 +8,7 @@
 
 #include <iostream>
 #include "AvlTree.h"
-#define Len(a) ((sizeof(a))/(sizeof(a[0])))
+#define Len(a) (sizeof(a)/sizeof(a[0]))
 using namespace std;
 static int arr[]= {3,2,1,4,5,6,7,16,15,14,13,12,11,10,8,9};
 int main() {
